751cc61d2507479ee4315d93a1a5835088b31538
adriancano89 <adrian.cano@inslapineda.cat>
2025-03-19 20:41:28 +0100

    Corrección error al obtener el carrito cuando el usuario no ha iniciado sesión

--- app/Http/Controllers/CarritoController.php
+++ app/Http/Controllers/CarritoController.php
@@ -26,10 +26,7 @@ class CarritoController extends Controller
         } else {
             $productosEnCarrito = [];
         }
-        // Si no se encuentra, asigna false
-        if ($productosEnCarrito == []) {
-            $productosEnCarrito = false;
-        }
+        
         $precioTotal = 0;
         foreach ($productosEnCarrito as $productoCarrito) {
             $precioTotal += $productoCarrito->producto->tipoProducto->precio * $productoCarrito->cantidad;
