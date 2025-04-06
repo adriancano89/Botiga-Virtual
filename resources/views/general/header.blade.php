dd29e4c712f134f095d28a995110902cdfea1203
adriancano89 <adrian.cano@inslapineda.cat>
2025-04-06 23:16:19 +0200

    Header responsive

--- resources/views/general/header.blade.php
+++ resources/views/general/header.blade.php
@@ -1,26 +1,31 @@
-<header class="flex flex-row items-center justify-between w-full fondo-primario text-white p-4">
-    <div class="ml-[10px]">
+<header class="flex flex-col sm:flex-row items-center justify-between w-full fondo-primario text-white p-4">
+    <div class="ml-[10px] mb-4 sm:mb-0">
         <a href="/" class="text-center">
-            <h1 class="font-bold text-[25px]">SUNDERO</h1>
-            <h2 class="text-[15px] mt-[-5px]">Sweatshirt</h2>
+            <h1 class="font-bold text-3xl">SUNDERO</h1>
+            <h2 class="text-lg mt-[-5px]">Sweatshirt</h2>
         </a>
     </div>
-    <nav>
-        <ul class="flex flex-row items-center justify-between gap-4 text-[20px]">
+    
+    <nav class="w-full sm:w-auto mb-4 sm:mb-0">
+        <ul class="flex flex-row items-center justify-center sm:justify-between gap-4 text-xl">
             <a href="/categorias/genero/hombre">
-                <li>Hombre</li>
+                <li class="apartado-header">Hombre</li>
             </a>
             <a href="/categorias/genero/mujer">
-                <li>Mujer</li>
+                <li class="apartado-header">Mujer</li>
             </a>
             <a href="/categorias/genero/niño">
-                <li>Niños</li>
+                <li class="apartado-header">Niños</li>
             </a>
         </ul>
     </nav>
-    <div class="flex flex-row gap-5 mr-[10px]">
-        <img src="{{asset('icons/general/lupa.png')}}" alt="buscar">
-        <a href="/carrito"><img src="{{asset('icons/general/carrito-de-compras.png')}}" alt="carrito"></a>
-        <a href="/profile"><img src="{{asset('icons/general/usuario.png')}}" alt="perfil"></a>
+
+    <div class="flex flex-row gap-5 sm:mr-4 mt-4 sm:mt-0 sm:ml-4">
+        <a href="/carrito">
+            <img src="{{asset('icons/general/carrito-de-compras.png')}}" alt="carrito" class="icono-header">
+        </a>
+        <a href="/profile">
+            <img src="{{asset('icons/general/usuario.png')}}" alt="perfil" class="icono-header">
+        </a>
     </div>
 </header>
